5bed082b4fb9a9a1db3a5408e4ff6f85c0cc6104
Lokeswaran Aruljothy <11lokeswaran@gmail.com>
2025-10-20 23:25:00 +0530

    🚀 feat: Setup Drizzle ORM

---
+++ env.ts
@@ -0,0 +1,11 @@
+const getEnv = (key: string) => {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Environment variable ${key} is not set`);
+  }
+  return value;
+};
+
+export const env = {
+  DATABASE_URL: getEnv('DATABASE_URL'),
+};
